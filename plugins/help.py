210b474341e0c96eac05ee3171a6daef27dcbaa2
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-08-24 19:43:38 +0100

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -40,8 +40,8 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-1):
         url = "https://t.me/HelpBdarija"
         button = [
-            [InlineKeyboardButton(text = 'ℹ Support Chat', url="https://t.me/DamienHelp")],
-            [InlineKeyboardButton(text = 'ℹ Channel', url=url)],
+            [InlineKeyboardButton(text = '📣 Support Chat 📣', url="https://t.me/DamienHelp")],
+            [InlineKeyboardButton(text = '🔔 Channel Updates 🔔', url=url)],
             [InlineKeyboardButton(text = '⬅️', callback_data = f"help+{pos-1}")]
         ]
     else:
